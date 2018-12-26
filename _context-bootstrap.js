d1a1060b36db8840f80855bbd5bd8d214e48c4ac
Puls Sovi <pulsovi@outlook.com>
2018-12-26 16:30:32 +0100

    premiere version fonctionnelle

--- _context-bootstrap.js
+++ _context-bootstrap.js
@@ -1,19 +1,87 @@
-var log;
-setTimeout(function() {
-  log = (function IIFE(){
-    var console = document.getElementById('context');
+var global = this;
 
-    return function(data){
-      console.appendChild(document.createTextNode(data));
-    };
-  }());
-  log('context-bootsrap starts\n');
+var log = (function IIFE() {
+  var console = document.getElementById('context');
 
-  function appendScript(src) {
-    var script = document.createElement('script');
-    script.src = src;
-    document.head.appendChild(script);
+  return function(data) {
+    console.appendChild(document.createTextNode(data));
+  };
+}());
+//log('context-bootsrap starts\n');
+
+function ThenableFront(parent) {
+  this.parent = parent;
+}
+
+ThenableFront.prototype.then = function then(cb) {
+  //log('new then <' + this.parent.id + '>\n');
+  if (this.parent.solved) {
+    this.parent.next.then(cb);
+  } else {
+    this.parent.thenCb.push(cb);
   }
+  return this;
+};
 
-  appendScript('_context.js');
-}, null);
+function Thenable(src) {
+  this.id = src;
+  this.thenCb = [];
+  this.front = new ThenableFront(this);
+  //log('new Thenable <' + this.id + '>\n');
+}
+
+Thenable.prototype.resolve = function(value) {
+  //log('resolve thenable <' + this.id + '> length <' + this.thenCb.length + '>\n');
+  var cbRetVal = this.thenCb.shift()
+    .call(this, value);
+  var cb = null;
+  if (this.thenCb.length) {
+    if (cbRetVal instanceof ThenableFront) {
+      //log('is instanceof\n');
+      while ((cb = this.thenCb.shift())) {
+        cbRetVal.then(cb);
+      }
+    } else {
+      //log('is not instanceof\n');
+    }
+  }
+  this.solved = true;
+  this.next = cbRetVal;
+};
+
+function appendScript(src) {
+  var thenable = new Thenable(src);
+  var script = document.createElement('script');
+  script.onload = function() {
+    //log('loaded <' + src + '>\n');
+    thenable.resolve(this);
+  };
+  document.head.appendChild(script);
+  script.src = src;
+  return thenable.front;
+}
+
+function main() {
+  //log('start work\n');
+  var context = new Context(global.context);
+  //log('context cree\n');
+  var collection = document.querySelectorAll('[context-data]');
+  var cible, contextContent;
+  for (var i = 0; i < collection.length; ++i){
+    cible = collection[i].getAttribute('context-data');
+    objCible = context.get(cible);
+    collection[i].innerText = objCible;
+  }
+}
+
+appendScript('_promise_polyfill.js')
+  .then(function() {
+    return appendScript('_Promise.js');
+  })
+  .then(function() {
+    return appendScript('_context.js');
+  })
+  .then(function() {
+    return appendScript('_get-context.js');
+  })
+  .then(main);
--- _context.js
+++ _context.js
@@ -6,25 +6,45 @@ var AJAX = {};
 
 // Method
 AJAX.get = function get(url) {
+  //log('AJAX.get <' + url + '>\n');
   var pnc = _.Promise.noCallBack();
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.onload = function() {
+    //log('xhr loaded\n');
     pnc.resolve.call(this, this.response);
   };
-  xhr.onerror = xhr.onabort = pnc.reject;
+  xhr.onerror = xhr.onabort = function(error) {
+    pnc.reject.call(this, error);
+  };
   xhr.send(null);
   return pnc.promise;
 };
 
 AJAX.getJSON = function getJSON(url) {
+  //log('AJAX.getJSON <' + url + '>\n');
   var pnc = _.Promise.noCallBack();
   this.get(url)
     .then(function(response) {
+      //log('AJAX loaded <' + url + '>\n');
+      //log('##############################\n');
+      //log(response);
+      //log('##############################\n');
       var json = JSON.parse(response);
       pnc.resolve.call(this, json);
     })
-    .catch(function() {
+    .catch(function(error) {
+      //log('AJAX error <' + arguments.length + '>\n');
+      //log('error <' + error + '>\n');
+      /**
+      for (var i in error) {
+        try {
+          log(i + '<' + error[i] + '>\n');
+        } catch (e) {
+          log(e + '\n');
+        }
+      }
+      /**/
       pnc.reject.call(this, arguments);
     });
   return pnc.promise;
@@ -35,14 +55,34 @@ AJAX.getJSON = function getJSON(url) {
 \*/
 
 var Tree = {};
-Tree.setParent = function setParent(obj, parent) {
+Tree.setRelation = function setRelation(obj, parent) {
   if (typeof parent !== 'undefined') {
+    obj.$ = obj.$ || {};
     obj.$.parent = parent;
   }
+
   var keys = Object.keys(obj);
+  if (~keys.indexOf("$")) {
+    keys.splice(keys.indexOf("$"), 1);
+  }
+
   for (var i = 0; i < keys.length; ++i) {
+    obj[keys[i]].$ = obj[keys[i]].$ || {};
+
+    if (0 === i) {
+      obj[keys[i]].$.previousSibling = null;
+    } else {
+      obj[keys[i]].$.previousSibling = obj[keys[i - 1]];
+    }
+
+    if (keys.length === keys[i + 1]) {
+      obj[keys[i]].$.nextSibling = null;
+    } else {
+      obj[keys[i]].$.nextSibling = obj[keys[i + 1]];
+    }
+
     if (typeof obj[keys[i]] === 'object') {
-      setParent(obj[keys[i]], obj);
+      setRelation(obj[keys[i]], obj);
     }
   }
 };
@@ -52,20 +92,34 @@ Tree.setParent = function setParent(obj, parent) {
 \*/
 
 function Context(contextObj) {
+  //log(contextObj + '\n');
   this.context = contextObj;
   this.init();
 }
 
 Context.prototype.init = function init() {
-  Tree.setParent(this);
+  //log('context init\n');
+  Tree.setRelation(this);
 };
 
 Context.prototype.get = function get(path) {
+  //log('get', path);
   var parts = path.split('.');
-  var elem = this;
+  var elem = this.context;
   for (var i = 0; i < parts.length; ++i) {
-    elem = elem[parts[i]];
+    //log(elem, parts[i]);
+    if (elem.hasOwnProperty(parts[i])) {
+      elem = elem[parts[i]];
+    } else if (elem.hasOwnProperty('$') && elem.$.hasOwnProperty(parts[i])) {
+      elem = elem.$[parts[i]];
+    } else {
+      //log("Erreur, l'element " + parts[i] + " est introuvable");
+    }
+    if (null === elem) {
+      return null;
+    }
   }
+  return elem;
 };
 
 /*\
@@ -90,13 +144,3 @@ Console.prototype.log = function(element) {
   }
   this.element.appendChild(element);
 };
-
-/*\
- * main
-\*/
-
-log('_context.js starts\n');
-/**
-var console = new Console(document.getElementById('context'));
-var context = new Context(AJAX.getJSON('_context.json'));
-/**/
