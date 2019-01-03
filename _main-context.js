97e100123039e63575f5430a636fe18902acc35a
Puls Sovi <pulsovi@outlook.com>
2019-01-03 06:13:49 +0100

    ajout de la gestion de l'attribut context-show pour conditionner l'apparition des balises contenant du context

--- _main-context.js
+++ _main-context.js
@@ -3,11 +3,23 @@
     log("Le context n'a pas pu être chargé.\nMerci de vérifier la syntaxe du fichier <_get-context.js>\n");
   }
   var context = new Context(global.context);
+
   var collection = document.querySelectorAll('[context-data]');
-  var cible, contextContent;
-  for (var i = 0; i < collection.length; ++i) {
+  var cible, contextContent, i;
+  for (i = 0; i < collection.length; ++i) {
     cible = collection[i].getAttribute('context-data');
     objCible = context.get(cible);
     collection[i].innerText = objCible;
   }
+
+  collection = document.querySelectorAll('[context-show]');
+  for (i = 0; i < collection.length; ++i) {
+    cible = collection[i].getAttribute('context-show');
+    objCible = context.get(cible);
+    if (!objCible) {
+      collection[i].style.display = 'none';
+    }
+  }
+
+  log("context chargé");
 }());
