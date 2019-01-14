0e124373618907536f7faa12f6c1be3bf9b8a3c4
Puls Sovi <pulsovi@outlook.com>
2019-01-14 13:15:20 +0100

    le breadcrumb fonctionne

--- set-context.js
+++ set-context.js
@@ -3,8 +3,30 @@ var setContext = angular.module('set-context', []);
 
 // Define the `breadcrumbController` controller on the `set-context` module
 setContext.controller('breadcrumbController', function breadcrumbController($scope) {
-  $scope.path = [];
-  $scope.data = context;
+  $scope.path = [{ id: 'context' }];
+  $scope.context = context;
+  $scope.setPath = function(index, key) {
+    // valeurs par defaut
+    index = index || 0;
+    if (!key) {
+      key = $scope.path[index].id;
+      --index;
+    }
+    // recuperer objet selectionne
+    var elem = $scope;
+    for (var i = 0; i <= index; ++i) {
+      elem = elem[$scope.path[i].id];
+    }
+    elem = elem[key];
+    // preparer le splice
+    var keys = Object.keys(elem)
+      .filter(function(k) {
+        return 'object' === typeof elem[k];
+      });
+    elem = { id: key, keys: keys };
+    $scope.path.splice(index + 1, $scope.path.length, elem);
+  };
+  $scope.setPath();
   /*
   le path contient des object de ce format
   {
