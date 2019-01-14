e136dd37394d6c633bea3ab7c1d3bee0d8ab2e16
Puls Sovi <pulsovi@outlook.com>
2019-01-14 11:51:25 +0100

    le titre ne contient que du texte affichable

--- _main-context.js
+++ _main-context.js
@@ -110,9 +110,9 @@
     var titleDeep = element.getAttribute('context-title-deep') || 1;
     var titleElem = document.createElement('code');
     var titleCible = element.getAttribute('context-quote');
-    var titleArr = [context.get(titleCible + '.title')];
+    var titleArr = [context.get(titleCible + '.title', { type: 'string' })];
     for (; titleDeep > 0; --titleDeep) {
-      titleArr.unshift(context.get(titleCible + '.id'));
+      titleArr.unshift(context.get(titleCible + '.id', { type: 'string' }));
       titleCible += '.parent';
     }
     titleElem.appendChild(document.createTextNode(titleArr.join(' ') + ':'));
